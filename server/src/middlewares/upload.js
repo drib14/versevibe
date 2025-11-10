aa8a83dae18c1498df87d0477d892de27c61b5c3
drib14 <jhondrib7@gmail.com>
2025-11-11 00:56:13 +0800

    setup

---
+++ server/src/middlewares/upload.js
@@ -0,0 +1,24 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "/tmp");
+  },
+  filename: (req, file, cb) => {
+    const uniqueName = Date.now() + path.extname(file.originalname);
+    cb(null, uniqueName);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only image files are allowed"), false);
+  }
+};
+
+const upload = multer({ storage, fileFilter });
+
+export default upload;
